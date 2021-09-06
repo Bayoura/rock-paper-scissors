700750dfeac6bb4ca0ff488acdacac07b9d77045
Bayoura <Kristin.Seidelmann@gmx.de>
2021-09-06 16:59:48 +0200

    main functionality of the game works

--- script.js
+++ script.js
@@ -1,90 +1,88 @@
+//Greetings, warrior! Welcome to the great Tournament! You will have to withstand five rounds of fighting against mighty opponents. Don't underestimate them, for they are unpredictable foes! For each new fight you may choose a new weapon.
+
+
+let userScore = 0;
+let computerScore = 0;
+
+//dom variables are indicated by the underscore 
+//caching the dom:
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_p = document.querySelector('.result p');
+const lance_div = document.getElementById('l');
+const sword_div = document.getElementById('s');
+const axe_div = document.getElementById('a');
+
+
+function main() {
+    lance_div.addEventListener('click', function () {
+        playRound('Lance', computerPlay);
+    });
+
+    sword_div.addEventListener('click', function () {
+        playRound('Sword', computerPlay);
+    });
+
+    axe_div.addEventListener('click', function () {
+        playRound('Axe', computerPlay);
+    });
+}
+
+main();
+
 function computerPlay() {
-    const gameOptions = ['ROCK', 'PAPER', 'SCISSORS'];
-    const computerChoice = Math.floor(Math.random() * gameOptions.length);
-    return gameOptions[computerChoice];
+    const gameOptions = ['Lance', 'Sword', 'Axe'];
+    const randomNum = Math.floor(Math.random() * gameOptions.length);
+    return gameOptions[randomNum];
 }
 
-let playerSelection;
-let computerSelection;
-
-/* ---- Old Version ----
-function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt('Type in either "Rock", "Paper" or "Scissors":', 'Rock');
-    playerSelection = playerSelection.toUpperCase();
-    computerSelection = computerPlay();
-
-    if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
-        return 'You won! Rock beats Scissors!';
-    } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
-        return 'You lost! Paper beats Rock!';
-    } else if (playerSelection == 'ROCK' && computerSelection == 'ROCK') {
-        return 'Draw! Both Rock!';   
-    }
-
-    if (playerSelection == 'PAPER' && computerSelection == 'ROCK') {
-        return 'You won! Paper beats Rock!';
-    } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
-        return 'You lost! Scissors beat Paper!';
-    } else if (playerSelection == 'PAPER' && computerSelection == 'PAPER') {
-        return 'Draw! Both Paper!';   
-    }
-
-    if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
-        return 'You lost! Rock beats Scissors!';
-    } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
-        return 'You won! Scissors beats Paper!';
-    } else if (playerSelection == 'SCISSORS' && computerSelection == 'SCISSORS') {
-        return 'Draw! Both Scissors!';   
-    } else {
-        return 'You have to choose either "Rock", "Paper" or "Scissors"!';
-    } 
-} 
-*/
-
-function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay();
-    playerSelection = prompt('Type in either "Rock", "Paper" or "Scissors":', 'Rock');
-    playerSelection = playerSelection.toUpperCase();
-    
-    if (playerSelection == computerSelection) {
-        return `Draw! Both ${playerSelection}!`;
-    } else if (
-        (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') ||
-        (playerSelection == 'PAPER' && computerSelection == 'ROCK') ||
-        (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') 
-    ) {
-        return `Yes!! You won! ${playerSelection} beats ${computerSelection}!`;
-    } else if (
-        (playerSelection == 'ROCK' && computerSelection == 'PAPER') ||
-        (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') ||
-        (playerSelection == 'SCISSORS' && computerSelection == 'ROCK')
-    ) {
-        return `Oh no! You lost... ${computerSelection} beats ${playerSelection}.`
-    } else {
-        return 'You have to choose either \'Rock\', \'Paper\' or \'Scissors\'!';
-    }
+function win(user, computer) {
+    userScore++;
+    userScore_span.textContent = userScore;
+    computerScore_span.textContent = computerScore;
+    result_p.innerHTML = `The Warrior\'s ${user} beats the Opponent\'s ${computer}!<br>
+    You have won this fight, warrior!`;
 }
 
+function lose(user, computer) {
+    computerScore++;
+    computerScore_span.textContent = computerScore;
+    userScore_span.textContent = userScore;
+    result_p.innerHTML = `The Warrior\'s ${user} loses to the Opponent\'s ${computer}!<br>
+    You have lost this fight, warrior!`;
+}
 
+function draw(user) {
+    result_p.innerHTML = `Both, Warrior and Opponent, have picked ${user}!<br>
+    The fight is undecided!`;
+}
+
+function playRound(userSelection, computerSelection) {
+    computerSelection = computerPlay();
+
+    if (userSelection == computerSelection) {
+        draw(userSelection);
+    } else if (
+        (userSelection == 'Lance' && computerSelection == 'Sword') ||
+        (userSelection == 'Sword' && computerSelection == 'Axe') ||
+        (userSelection == 'Axe' && computerSelection == 'Lance')
+    ) {
+        win(userSelection, computerSelection);
+    } else if (
+        (userSelection == 'Lance' && computerSelection == 'Axe') ||
+        (userSelection == 'Sword' && computerSelection == 'Lance') ||
+        (userSelection == 'Axe' && computerSelection == 'Sword')
+    ) {
+        lose(userSelection, computerSelection);    
+    }
+
+}
 
 /*
 function game() {
     for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerSelection));
+        console.log(playRound(userSelection, computerSelection));
     }
 }
 */
-
-const body = document.body;
-const div = document.createElement('div');
-const btn1 = document.createElement('button');
-btn1.textContent = 'Rock';
-const btn2 = document.createElement('button');
-btn2.textContent = 'Paper';
-const btn3 = document.createElement('button');
-btn3.textContent = 'Scissors';
-div.append(btn1, btn2, btn3);
-body.append(div);
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', playRound));
